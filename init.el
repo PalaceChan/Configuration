eec9283846a82faa7a7c3b150090cc47c2ef4d7c
YYY <XXX>
2020-08-24 00:10:44 -0500

    update melpa path

--- init.el
+++ init.el
@@ -3,7 +3,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
 (defvar avelazqu/default-install-packages
@@ -36,5 +36,11 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-bullets magit dmenu which-key irony company-irony exwm wrap-region company dired-subtree ess use-package)))
+    (clj-refactor clojure-mode flycheck company-lsp lsp-mode phi-search-mc multiple-cursors helm-descbinds iedit dumb-jump erc-colorize crux speed-type auto-package-update vlf discover-my-major company-posframe company-shell python-black easy-kill dired-git-info helm-ag helm-rg helm-projectile projectile rmsbolt irony-eldoc company-c-headers leetcode yasnippet-snippets undo-tree visual-fill-column paredit deadgrep ztree fancy-narrow elfeed doom-themes org-bullets magit dmenu which-key irony company-irony exwm wrap-region company dired-subtree ess use-package)))
  '(send-mail-function (quote sendmail-send-it)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
