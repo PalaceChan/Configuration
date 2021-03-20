51071cd3991311de1274ced9e73c585eeca37638
YYY <XXX>
2021-03-20 11:58:19 -0500

    few updates and cleanup
    - new buffer section with
    - world time format change
    - dired default hide details mode
    - disable company-lsp as off melpa

--- init.el
+++ init.el
@@ -1,17 +1,18 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                                         BOOTSTRAP
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
-(defvar avelazqu/default-install-packages
+(defvar ava/default-install-packages
   (list 'use-package 'bind-key 'diminish)
   "Packages that should be installed by default.")
 (unless package-archive-contents
   (package-refresh-contents))
-(dolist (package avelazqu/default-install-packages)
+(dolist (package ava/default-install-packages)
   (unless (package-installed-p package)
     (package-install package)))
 (require 'bind-key)
@@ -35,7 +36,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vterm gcmh clj-refactor clojure-mode flycheck company-lsp lsp-mode phi-search-mc multiple-cursors helm-descbinds iedit dumb-jump erc-colorize crux speed-type auto-package-update vlf discover-my-major company-posframe company-shell python-black easy-kill dired-git-info helm-ag helm-rg helm-projectile projectile rmsbolt irony-eldoc company-c-headers leetcode yasnippet-snippets undo-tree visual-fill-column paredit deadgrep ztree fancy-narrow elfeed doom-themes org-bullets magit dmenu which-key irony company-irony exwm wrap-region company dired-subtree ess use-package))
+   '(helpful org-mime company-auctex auctex vterm gcmh clj-refactor clojure-mode flycheck company-lsp lsp-mode phi-search-mc multiple-cursors helm-descbinds iedit dumb-jump erc-colorize crux speed-type auto-package-update vlf discover-my-major company-posframe company-shell python-black easy-kill dired-git-info helm-ag helm-rg helm-projectile projectile rmsbolt irony-eldoc company-c-headers leetcode yasnippet-snippets undo-tree visual-fill-column paredit deadgrep ztree fancy-narrow elfeed doom-themes org-bullets magit dmenu which-key irony company-irony exwm wrap-region company dired-subtree ess use-package))
  '(send-mail-function 'sendmail-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
