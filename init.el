6dae624c78228c6542345700ae1c9497ed1f932e
PalaceChan <XXX>
2024-12-22 23:26:26 -0500

    banish customization stuff from init.el to custom.el

--- init.el
+++ init.el
@@ -34,18 +34,5 @@
 (when (file-readable-p "~/.emacs.d/config.org")
   (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-show-quick-access t nil nil "Customized with use-package company")
- '(inferior-ess-r-program "/usr/bin/R" nil nil "Customized with use-package ess")
- '(package-selected-packages
-   '(ztree yasnippet-snippets ws-butler wrap-region which-key wgrep-helm wgrep-deadgrep vterm vlf visual-fill-column use-package undo-tree tree-sitter-langs transpose-frame sqlite3 speed-type smart-comment rmsbolt python-black phi-search-mc pdf-tools orgit org-noter org-modern org-mime org-appear move-text mixed-pitch minions lsp-ui leetcode json-mode jq-mode irony-eldoc iedit ibuffer-vc helpful helm-swoop helm-rg helm-projectile helm-org-rifle helm-org helm-git-grep helm-descbinds helm-dash helm-company helm-ag gptel git-timemachine git-gutter gcmh forge flycheck-clang-tidy fancy-narrow expand-region ess erc-colorize ement emacsql-sqlite elpy elfeed eldoc-stan eglot editorconfig easy-kill dumb-jump doom-themes doom-modeline dmenu discover-my-major dired-subtree dired-rsync dired-recent dired-git-info diminish deadgrep crux company-stan company-shell company-posframe company-irony company-c-headers company-auctex clj-refactor clang-format auto-package-update ace-window ace-link)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file) (load custom-file))
